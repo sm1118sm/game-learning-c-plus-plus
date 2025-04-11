386ea85a5e076d095ac570ac44b86596fc105e4b
이상민 <sm1118sm@gmail.com>
2025-04-11 12:55:46 +0900

    Add files via upload

---
+++ c++ 프로그래밍 과제 06/c++ 이상민/c++ 이상민 puzzlegame class/bonus.h
@@ -0,0 +1,11 @@
+//#include "RankingBoard.h"
+//#include <stdlib.h>
+//#include <time.h>
+//
+//int main() {
+//	RankingBoard board;
+//	board.load("ranking.bin");
+//	board.print();
+//	board.store("ranking.bin");
+//}
+//
---
+++ c++ 프로그래밍 과제 06/c++ 이상민/c++ 이상민 puzzlegame class/main.cpp
@@ -0,0 +1,9 @@
+#include "RankingBoard.h"
+#include "FifteenPuzzle.h"
+
+int main()
+{
+
+	FifteenPuzzle game; 
+	game.play("ranking.bin"); //게임 시작
+}
